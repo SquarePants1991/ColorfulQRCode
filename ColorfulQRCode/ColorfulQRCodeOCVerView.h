f5560d6d7a44b83e4dcf4208716260854f3fe877
wangyang <yang1.wang@baidao.com>
2017-08-18 10:23:53 +0800

    add oc ver CALayer impl

---
+++ ColorfulQRCode/ColorfulQRCodeOCVerView.h
@@ -0,0 +1,14 @@
+//
+//  ColorfulQRCodeOCVerView.h
+//  ColorfulQRCode
+//
+//  Created by wang yang on 2017/8/17.
+//  Copyright © 2017年 ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorfulQRCodeOCVerView : UIView
+- (void)syncFrame;
+- (void)setQRCodeImage:(UIImage *)qrcodeImage;
+@end
--- ColorfulQRCode/ViewController.swift
+++ ColorfulQRCode/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var qrcodeView: ColorfulQRCodeView!
-    @IBOutlet weak var qrcodeMetalView: ColorfulQRCodeMetalView!
+    @IBOutlet weak var qrcodeOCVerView: ColorfulQRCodeOCVerView!    // OC版本
+    @IBOutlet weak var qrcodeView: ColorfulQRCodeView!              // Swift版本
+    @IBOutlet weak var qrcodeMetalView: ColorfulQRCodeMetalView!    // Swift+Metal
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.init(white: 0.85, alpha: 1.0)
         if let image = UIImage.init(named: "qrcode.png") {
+            qrcodeOCVerView.setQRCodeImage(image)
             qrcodeView.setQRCodeImage(qrcodeImage: image)
             qrcodeMetalView.setQRCodeImage(qrcodeImage: image)
         }
@@ -24,6 +26,7 @@ class ViewController: UIViewController {
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        qrcodeOCVerView.syncFrame()
         qrcodeView.syncFrame()
         qrcodeMetalView.syncFrame()
     }
